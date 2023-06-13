7b5cc9cf512d249346e0a911873c7136e2697e3e
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-06-13 12:38:39 +0900

    header layout end

--- Finmate/resources/views/layout/header.blade.php
+++ Finmate/resources/views/layout/header.blade.php
@@ -1,7 +1,8 @@
 <header>
     <div class="headerlogo">
 
-        <img src="" alt="">
+        <img src="{{ asset('/img/logo1.png') }}" alt="로고">
+
         <p>FinMate</p>
     </div>
     <div class="headerMain">
@@ -11,16 +12,11 @@
 
             {{-- <a href="{{ route('boards.index') }}" class="nav-item {{ Request::is('users/edit') ? '' : 'is-active' }}" active-color="red">홈으로</a>
             <a href="{{ route('users.edit') }}" class="nav-item {{ Request::is('users/edit') ? 'is-active' : '' }}" active-color="blue">회원정보 수정</a>
-
             <a href="{{ route('users.logout') }}" class="nav-item {{ Request::is('users/logout') ? 'is-active' : '' }}" active-color="green">로그아웃</a> --}}
 
             <a href="" class="nav-item is-active" active-color="#FF7676">main</a>
             <a href="" class="nav-item is-active" active-color="#FF7676">sign up</a>
             <a href="" class="nav-item is-active" active-color="#FF7676">login</a>
-
-
-
-
 
             <span class="nav-indicator"></span>
         </nav>
@@ -32,11 +28,7 @@
         <a href="" class="nav-item is-active" active-color="#FF7676">sign up</a>
         <a href="" class="nav-item is-active" active-color="#FF7676">login</a>
 
-
-
-
             {{-- <a href="{{ route('users.login') }}" class="nav-item {{ Request::is('users/login') ? 'is-active' : '' }}" active-color="red">로그인 페이지</a>
-
             <a href="{{ route('users.registration') }}" class="nav-item {{ Request::is('users/registration') ? 'is-active' : '' }}" active-color="blue">회원가입</a> --}}
 
             <span class="nav-indicator"></span>
--- Finmate/resources/views/main.blade.php
+++ Finmate/resources/views/main.blade.php
@@ -9,6 +9,9 @@
 @section('contents')
 
 <h1>메인 페이지 콘텐츠 영역입니다.</h1>
+<br>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut repellendus, at architecto maxime aliquam nobis exercitationem ab. Odio, consequuntur culpa ipsa fuga laborum perspiciatis perferendis saepe velit maxime pariatur tempore.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem similique odio laborum minima perspiciatis nobis esse corrupti error, consequuntur et doloribus veritatis iste fugit provident accusantium facere eaque sit labore.</p>
 
 
 @endsection
